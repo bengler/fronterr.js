223aaa31ec0e1c17f655efe97d018865c8d5751c
Bjørge Næss <bjoerge@bengler.no>
2013-01-18 14:55:03 +0100

    Misc.

--- README.md
+++ README.md
@@ -9,7 +9,16 @@ var services = new pebbles.service.ServiceSet({host: 'my.pebbles.host.org'}).use
   fronterr: 1
 });
 
-var fronterr = new FrontErr({service: services.fronterr, airbrakeAPI: '2.3'});
+var fronterr = new FrontErr({
+  app: {
+    name: 'my-app',
+    version: '0.0.1'
+  },
+  airbrakeAPI: '2.3',
+  environment: process.env.NODE_ENV,
+  service: services.fronterr
+});
+
 window.onerror = function(message, file, line) {
   fronterr.reportError({message: message, file: file, line: line}).then(function() {
     console.log("Error report submitted to fronterr");
--- fronterr.js
+++ fronterr.js
@@ -1,24 +1,64 @@
 var stacktrace = require("stacktrace-js");
 var AirbrakeNotice = require("airbrake-notice");
+var fronterr = require("../package");
+
 
 function FrontErr(opts) {
   opts || (opts = {});
   if (!opts.service) throw Error("Please provide a FrontErr service endpoint");
-  if (!opts.version) throw Error("Please provide a Airbrake API version");
+  if (!opts.airbrakeAPI) throw Error("Please provide a Airbrake API version");
+
+  this.app = {};
+  this.app.name = (opts.app && opts.app.name);
+  this.app.version = (opts.app && opts.app.version);
+
+  this.environment = opts.environment;
 
   this.service = opts.service;
-  this.notice = new AirbrakeNotice(opts.version);
+  this.notice = new AirbrakeNotice(opts.airbrakeAPI);
 }
 
-FrontErr.prototype.createNotice = function(error) {
-
-  this.notice.create({
-
-  });
+FrontErr.prototype.createBaseNotice = function() {
+  return {
+    apiKey: 'API_KEY_FOR:'+this.app.name,
+    notifier: {
+      name: 'fronterr.js',
+      version: fronterr.version,
+      url: fronterr.homepage || (fronterr.repository && fronterr.repository .url) || ''
+    },
+    serverEnvironment: {
+      name: this.environment || 'n/a',
+      projectRoot: this.path || document.location.pathname,
+      appVersion: this.app.version || 'n/a'
+    },
+    request: {
+      url: document.location.href,
+      component: 'client-javascript',
+      cgiData: {
+        SERVER_NAME: document.location.host,
+        HTTP_USER_AGENT: window.navigator.userAgent
+      }
+    }
+  };
 };
+
+FrontErr.prototype.createNotice = function(error) {
+  var notice = this.createBaseNotice();
+  notice.error = {
+    class: 'error',
+    message: error.message,
+    backtrace: [{number: 10, file:"internal:", method: ""}]
+  };
+  return this.notice.create(notice);
+};
+
 FrontErr.prototype.reportError = function(error) {
   return this.sendNotice(this.createNotice(error));
 };
+
 FrontErr.prototype.sendNotice = function(notice) {
-  this.service.post("/notifier_api/v2/notices.xml", {notice: this.notice.create(error)});
+  console.log('posting', notice);
+  return this.service.post("/notifier_api/v2/notices.xml", {data: notice});
 };
+
+module.exports = FrontErr;
